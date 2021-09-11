a7a5aea3c613d752f6bd8391bc047da1889e821e
Rafiou Sitou <rafiousitou90@gmail.com>
2021-09-11 18:11:27 -0300

    feat(prova-demo-blog): Initial commit

---
+++ prova/prova-demo-blog/src/main/java/com/rafdev/prova/demo/blog/ProvaDemoBlogApplication.java
@@ -0,0 +1,13 @@
+package com.rafdev.prova.demo.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaDemoBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProvaDemoBlogApplication.class, args);
+    }
+
+}
---
+++ prova/prova-demo-blog/src/test/java/com/rafdev/prova/demo/blog/ProvaDemoBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafdev.prova.demo.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaDemoBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
